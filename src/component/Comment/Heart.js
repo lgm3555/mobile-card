497ad4d24f0a1dbd6ea23bcd9b253e34963e2311
LimGwangMin <lgm3555@gmail.com>
2022-05-14 17:27:52 +0900

    create like

--- src/component/Comment/Heart.js
+++ src/component/Comment/Heart.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import "./Heart.scss";
+import axios from 'axios';
+import DeviceInfo from 'react-native-device-info';
 
 const Heart = (props) => {
 
   const flag = props.deviceId;
   const [like, setLike] = useState(Boolean(flag));
 
-  const handleClick = () => {
+  const handleClick = async () => {
       if (like && flag) {
           return;
+      }
+      try {
+        const deviceId = DeviceInfo.getUniqueId().then(uniqueId => {
+          return uniqueId;
+        });
+
+        const getApiResult = await axios.post('/like', {
+          "deviceId": deviceId
+        });
+      } catch (error) {
+        console.log(error);
       }
       setLike(true);
     };
